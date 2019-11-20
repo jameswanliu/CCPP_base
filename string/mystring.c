7fd0ba941b6a0da7b91f6296b7cc0d8c810fba5c
jameswanliu <952121091@qq.com>
2019-11-20 19:27:57 +0800

    添加功能

--- string/mystring.c
+++ string/mystring.c
@@ -4,13 +4,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int mystrlen(char *str) {
     int count = 0;
 
     while (*str != '\0') {
         count++;
-        str++;
+        str++;//指针移动
     }
     return count;
 }
@@ -18,8 +19,8 @@ int mystrlen(char *str) {
 
 int main() {
 
-    char array[] = {'s', 's', 's', '\0'};
-    printf("数组长度为%d\n",mystrlen(array));
+    char array[] = {'s', 's', 's', 's', 's', '\0'};
+    printf("数组长度为%d\n", mystrlen(array));
 
 //
 //    array[1] = 'b';
@@ -28,14 +29,22 @@ int main() {
 
 
 
-    char* string = "shish";//字符串常量 不可修改?
-    string ="hahaha";
-    printf("修改后的string为%s\n",string);
+    char *string = "fadfa";//字符串常量 不可修改?
+    string = "fadf";//string 指针指向新的内容
+    printf("修改后的string为%s\n", string);
     const int sum = 100;
-    printf("修改前的sum =%d\n",sum);
-    int* p = &sum;
+    printf("修改前的sum =%d\n", sum);
+    int *p = &sum;//c编译器bug
     *p = 200;
-    printf("修改后的sum =%d\n",sum);
+    printf("修改后的sum =%d\n", sum);
+
+    printf("字符串比较 cmp = %d\n", cmp(array, string));
+
     return 0;
 
+}
+
+
+int cmp(char *str, char *str2) {
+    return strcmp(str, str2); //aaa hello
 }
